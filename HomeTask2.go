4a17c2d3845910dd8d7d4c374a224106c5097b29
Mark Voronov <Follover@yandex.ru>
2024-03-31 12:32:52 +0300

    «home_task2»

---
+++ HomeTask2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	quotient := 16 / 3
+	remainder := 16 % 3
+	fmt.Printf("Результат: %d, остаток от деления: %d, тип результата: %T", quotient, remainder, quotient)
+
+}
